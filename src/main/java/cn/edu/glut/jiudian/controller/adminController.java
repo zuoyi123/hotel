bda58719161bfb4cab9898ead7ef82871c2a2264
zuoyi <2241303893@qq.com>
2023-04-16 22:51:46 +0800

    update

---
+++ src/main/java/cn/edu/glut/jiudian/controller/adminController.java
@@ -0,0 +1,14 @@
+package cn.edu.glut.jiudian.controller;
+
+import cn.edu.glut.jiudian.service.UserService;
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author stone(huangshizhang) at 2019-06-12 11:24
+ */
+@Controller
+public class adminController {
+    //ddddd
+    private UserService userService;
+
+}
